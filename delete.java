00fc56383fbd1da1c70c314faa1828240adbea57
Simplelearngit <101037160+Simplelearngit@users.noreply.github.com>
2024-03-26 20:07:51 +0200

    Update delete.java

--- delete.java
+++ delete.java
@@ -1,28 +1,35 @@
-public class employeeInsert {
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class employeedelete {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		// 1.load the driver
 
-		Class.forName("com.mysql.cj.jdbc.Driver");
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				
+				// 2.establish connection
+				Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/employee", "root", "root");
 
-		// 2.establish connection
-		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/employee", "root", "root");
+				// 3.create statement
+				Statement statement = connection.createStatement();
 
-		// 3.create statement
-		Statement statement = connection.createStatement();
+				// 4.execute statement and process result
+				int result = statement.executeUpdate("delete from employee where empid=2");
 
-		// 4.execute statement and process result
-		int result = statement.executeUpdate("insert into employee values(1,'Nilam','Pune'),(2,'Kajal','karjat'),(4,'Nikita','kashti')");
+				if (result != 0) {
+					System.out.println("Data deleted");
+				} else {
+					System.out.println("Data not deleted");
+				}
 
-		if (result != 0) {
-			System.out.println("Data inserted");
-		} else {
-			System.out.println("Data not inserted");
-		}
+				// 5.close connection
+				connection.close();
 
-		// 5.close connection
-		connection.close();
 
 	}
-
 }
